59a18fbfee1d0b431ffe3502b85f05986275c90d
Kirill Shatskiy <shaackiy@gmail.com>
2018-04-15 07:43:47 +0300

    Add README.md. Update server. Install device.

--- server/index.js
+++ server/index.js
@@ -3,15 +3,21 @@ const device = require('device');
 const path = require('path');
 
 const app = express();
+const PORT = 3000;
 
+/**
+ * It's a demo server. In production environment cookies should be used to
+ * avoid detecting device on each request.
+ */
 app.use((req, res, next) => {
     const url = req.url;
     const userAgent = req.header('user-agent');
     const { type } = device(userAgent);
     req.url = `${type}${url}`;
-    
     next('route');
 });
 
 app.use(express.static(path.join(__dirname, '../builds')))
-app.listen(3000);
+app.listen(PORT, () => {
+    console.log(`App listens on ${PORT}`);
+});
